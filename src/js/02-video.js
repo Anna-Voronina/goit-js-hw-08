7001148be7360c4c8c5e2b96ef371dbacc0ae340
Anna-Voronina <anna.vs2512@gmail.com>
2023-05-27 12:55:17 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,15 +2,25 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 import localStorage from './localstorage.js';
 
-const iframe = document.querySelector('iframe');
+const iframe = document.querySelector('#vimeo-player');
 
 const player = new Player(iframe);
 const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+setTime();
+
+function setTime() {
+  const currentTimeData = localStorage.load(CURRENT_TIME_KEY);
+
+  if (currentTimeData === undefined) {
+    return;
+  }
+
+  player.setCurrentTime(currentTimeData);
+}
 
 player.on('timeupdate', throttle(onTimeUpdate, 1000));
 
 function onTimeUpdate(data) {
   localStorage.save(CURRENT_TIME_KEY, data.seconds);
 }
-
-player.setCurrentTime(localStorage.load(CURRENT_TIME_KEY));
