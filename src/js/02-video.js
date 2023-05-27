4941f6ac0132652cf0417e5778e39235eaa7da33
Anna-Voronina <anna.vs2512@gmail.com>
2023-05-27 10:56:04 +0300

    Update task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import localStorage from './localstorage.js';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(data) {
+  localStorage.save(CURRENT_TIME_KEY, data.seconds);
+}
+
+player.setCurrentTime(localStorage.load(CURRENT_TIME_KEY));
